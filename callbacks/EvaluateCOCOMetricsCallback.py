5c1c69b7c45059505f20202a82693df33ac0999a
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2026-02-18 16:46:43 -0300

    :construction: Progreso para que el entrenamiento ande

---
+++ callbacks/EvaluateCOCOMetricsCallback.py
@@ -0,0 +1,41 @@
+from keras.callbacks import Callback
+import keras_cv
+
+class EvaluateCOCOMetricsCallback(Callback):
+    """
+    Docstring para EvaluateCOCOMetricsCallback
+    Al final de cada epoca se evaluan las metricas MaP e IoU,
+    relacionadas al conjunto de datos COCO. Si el valor de MaP
+    mejora se guarda el modelo en la ruta especificada.
+    """
+    
+    def __init__(self, data, save_path):
+        super().__init__()
+        self.data = data
+        self.metrics = keras_cv.metrics.BoxCOCOMetrics(
+            bounding_box_format="rel_xywh",
+            evaluate_freq=1e9,
+        )
+
+        self.save_path = save_path
+        self.best_map = -1.0
+
+    def on_epoch_end(self, epoch, logs):
+        self.metrics.reset_state()
+        for batch in self.data:
+            images, y_true = batch[0], batch[1]
+            y_pred = self.model.predict(images, verbose=0)
+            self.metrics.update_state(y_true, y_pred)
+
+        metrics = self.metrics.result(force=True)
+        logs.update(metrics)
+
+        # Si MaP mejora guarda el modelo
+        current_map = metrics["MaP"]
+        if current_map > self.best_map:
+            self.best_map = current_map
+            self.model.save(self.save_path)  
+
+        return logs
+
+
--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import os
 #os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"    # Ocultar mensajes
 from tqdm.auto import tqdm
+from callbacks.EvaluateCOCOMetricsCallback import EvaluateCOCOMetricsCallback
 from utils.loadFiles import load_dataset, txt_to_annotation
 
 
 import tensorflow as tf
+from keras.optimizers import Adam
 
 from utils.parse import center_rel_xywh_to_rel_xywh
 from utils.resize import resize_rel_center_xywh
 from utils.visualize import visualize_dataset
+
+import keras_cv
 # from keras_cv import bounding_box
 # from keras_cv import visualization
+
 
 SPLIT_RATIO = 0.2
 BATCH_SIZE = 4
 LEARNING_RATE = 0.001
 EPOCH = 5
+GLOBAL_CLIPNORM = 10.0
+SAVE_PATH = 'models/model.keras'
 
 class_ids = [
     "observation"
@@ -23,8 +30,8 @@ class_ids = [
 class_mapping = dict(zip(range(len(class_ids)), class_ids))
 
 # Ubicacion de las imagenes y anotaciones
-path_images = "D:\\Datasets\\conGSSSP\\images\\"
-path_annot = "D:\\Datasets\\conGSSSP\\labels\\"
+path_images = "/Users/s.a.p.a/Documents/Datasets/conGSSSP/images/" # "D:\\Datasets\\conGSSSP\\images\\"
+path_annot = "/Users/s.a.p.a/Documents/Datasets/conGSSSP/labels/" # "D:\\Datasets\\conGSSSP\\labels\\"
 
 # Recuperar archivos TXT de etiquetas ordenados
 txt_files = sorted(
@@ -59,9 +66,21 @@ for txt_file in tqdm(txt_files):
     bbox.append(boxes)
     classes.append(class_ids)
 
+# Manejo de casos vacios
+processed_boxes = []
+for b in bbox:
+    if len(b) == 0:
+        processed_boxes.append(tf.zeros((0, 4), dtype=tf.float32))
+    else:
+        processed_boxes.append(tf.convert_to_tensor(b, dtype=tf.float32))
+
+
 # Cajas, Clases y Caminos de imagenes mejor como tensores
-bbox = tf.ragged.constant(bbox)
+bbox = tf.ragged.constant(processed_boxes)
+print('___________')
+print(bbox.shape)
 classes = tf.ragged.constant(classes)
+print(bbox.shape)
 image_paths = tf.ragged.constant(image_paths)
 
 # Dataset final
@@ -99,4 +118,47 @@ visualize_dataset(
 visualize_dataset(
     val_ds, bounding_box_format="rel_xywh", value_range=(0, 255), 
     rows=2, cols=2, class_mapping=class_mapping, save_path="val_visualization.png"
+)
+
+### Tuplas para el entrenamiento ###
+# Funcion
+def dict_to_tuple(inputs):
+    return inputs["images"], inputs["bounding_boxes"]
+# Entrenamiento
+train_ds = train_ds.map(dict_to_tuple, num_parallel_calls=tf.data.AUTOTUNE)
+train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
+# Validacion
+val_ds = val_ds.map(dict_to_tuple, num_parallel_calls=tf.data.AUTOTUNE)
+val_ds = val_ds.prefetch(tf.data.AUTOTUNE)
+
+### Modelo ###
+# Esqueleto YOLO
+backbone = keras_cv.models.YOLOV8Backbone.from_preset(
+    "yolo_v8_s_backbone_coco"  # small backbone con pesos de COCO
+)
+# Modelo
+yolo = keras_cv.models.YOLOV8Detector(
+    num_classes=len(class_mapping),
+    bounding_box_format="rel_xywh",
+    backbone=backbone,
+    fpn_depth=1,
+)
+# Optimizador 
+optimizer = Adam(
+    learning_rate=LEARNING_RATE,
+    global_clipnorm=GLOBAL_CLIPNORM
+)
+# Compilar
+yolo.compile(
+    optimizer=optimizer,
+    classification_loss="binary_crossentropy",
+    box_loss='ciou'
+)
+
+# Entrenar
+yolo.fit(
+    train_ds,
+    validation_data=val_ds,
+    callbacks=[EvaluateCOCOMetricsCallback(val_ds, SAVE_PATH)],
+    epochs=EPOCH
 )
